1a906645e3e6f3704c2bade704e96479038eb39a
Ruslan Tumasoff <Rus-nura@yandex.ru>
2022-02-13 17:54:08 +0700

    @EnableSchedule added to main class

--- src/main/java/ru/tumasoff/demo/monitoring/MonitoringApplication.java
+++ src/main/java/ru/tumasoff/demo/monitoring/MonitoringApplication.java
@@ -2,12 +2,12 @@ package ru.tumasoff.demo.monitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MonitoringApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MonitoringApplication.class, args);
 	}
-
 }
